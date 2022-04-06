a2499c47d12e25a39aecfd75f067f15acc23f104
Ilan-J <ilan.jaglin@viacesi.fr>
2022-04-06 16:39:18 +0200

    ftgyutyfyuu

--- angular/tpMessagerie/src/app/app.component.ts
+++ angular/tpMessagerie/src/app/app.component.ts
@@ -6,12 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'toDoList';
-
-  todoList: string[] = [];
-  newTodo: string = '';
-
-  createTodo() {
-    
-  }
+  title = 'tpMessagerie';
 }
--- angular/tpMessagerie/src/app/app.module.ts
+++ angular/tpMessagerie/src/app/app.module.ts
@@ -2,14 +2,10 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { EnTeteComponent } from './en-tete/en-tete.component';
-import { PiedDePageComponent } from './pied-de-page/pied-de-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    EnTeteComponent,
-    PiedDePageComponent
+    AppComponent
   ],
   imports: [
     BrowserModule
