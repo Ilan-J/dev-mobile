46ac333192c8d2396691e4bc42fefd9fcad8905a
Ilan-J <ilan.jaglin@viacesi.fr>
2022-04-07 12:15:19 +0200

    tp msg

--- angular/tpMessagerie/src/app/app.component.ts
+++ angular/tpMessagerie/src/app/app.component.ts
@@ -7,4 +7,36 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'tpMessagerie';
+
+
+  userList: string[] = [];
+
+  senderUser: string = '';
+  messageSent: string = '';
+
+
+  msgList: Msg[] = [];
+
+  sendMsg(): void {
+
+    if(this.senderUser == "" || this.messageSent == "") {
+      return;
+    }
+
+    if(this.msgList.find((msg) => msg.user == this.senderUser) == undefined) {
+      
+      this.userList.push(this.senderUser);
+    }
+
+    this.msgList.push(new Msg(this.senderUser, this.messageSent));
+  }
+}
+
+class Msg{
+
+  constructor(
+    public user: string,
+    public message: string,
+    public date: Date = new Date()
+  ) { }
 }
